552300ef0718c5adf4c3d323da118e21e394e2a9
Rodrigo Cabral <rodrigo.cabral_ti@live.com>
2025-05-29 22:19:11 -0400

    No ramo develop
    * Mudanças a serem submetidas:
    	modificado:    routes/web.php
    
    @rodrigocabral78 Rodrigo Cabral <rodrigo.cabral_ti@live.com>

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,13 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    // return view('welcome');
+    return 'Olá, Mundo!';
+});
+
+Route::get('/sobre', function () {
+    return 'Sobre nós';
+});
+Route::get('/contato', function () {
+    return 'Entre em contato';
 });
